d3ed9e06dd1761118ba2043cb3c4c329b88e2f12
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2022-12-20 22:07:49 +0100

    Configure database settings.

--- taems/settings.py
+++ taems/settings.py
@@ -82,8 +82,11 @@ WSGI_APPLICATION = 'taems.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'taems',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'I$bth@nbtt0026'
     }
 }
 
