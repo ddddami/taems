f03e7c0525291d074040ae0d36268620e5044ad6
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-01-03 20:41:14 +0100

    Register the custom User Model in the core app.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
+    )
