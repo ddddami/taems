83394d5b1c3f2a747d63bb97cc49b3b8636685f2
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-01-12 22:02:58 +0100

    Reference first_name and last_name field from user in Student

--- school/models.py
+++ school/models.py
@@ -52,8 +52,6 @@ class Student(models.Model):
         ('M', 'Male'),
         ('F', 'Female')
     ]
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
     sex = models.CharField(max_length=1, choices=SEX_CHOICES)
     birth_date = models.DateField()
     _class = models.ForeignKey(Class, on_delete=models.PROTECT)
@@ -64,7 +62,13 @@ class Student(models.Model):
                                 on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    def first_name(self):
+        return self.user.first_name
+
+    def last_name(self):
+        return self.user.last_name
 
 
 class Teacher(models.Model):
