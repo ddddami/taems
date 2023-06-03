9bef098e7ac3f759cf39580ff2b9760ae5c0e883
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-06-03 08:53:15 +0100

    Remove the code field from State model.

--- location/models.py
+++ location/models.py
@@ -6,8 +6,13 @@ from django.db import models
 class State(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class LGA(models.Model):
     name = models.CharField(max_length=255)
-    code = models.CharField(max_length=7, unique=True, default='Z')
     state = models.ForeignKey(State, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.name
