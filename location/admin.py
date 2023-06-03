d806b4091a23f4a6dff186f34befc78d8ddb274c
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-06-03 09:17:47 +0100

    Register state and lga admin in location app.

--- location/admin.py
+++ location/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from django.db.models.aggregates import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+from .models import State, LGA
 # Register your models here.
+
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'lgas')
+
+    def lgas(self, state):
+        url = (
+            reverse('admin:location_lga_changelist')
+            + '?'
+            + urlencode({
+                'state__id': str(state.id)
+            }))
+
+        return format_html('<a href="{}">{} LGAs</a>', url, state.lgas_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(lgas_count=Count('lga'))
+
+
+@admin.register(LGA)
+class LGAAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'state')
