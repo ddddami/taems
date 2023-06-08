864c396b182cbf4d26ab5ac09162f4afe88d6276
Damilola Osunsanwo <oluwadamilola013@gmail.com>
2023-06-08 21:49:38 +0100

    Create the Address model.

--- location/admin.py
+++ location/admin.py
@@ -2,8 +2,16 @@ from django.contrib import admin
 from django.db.models.aggregates import Count
 from django.utils.html import format_html, urlencode
 from django.urls import reverse
-from .models import State, LGA
+from .models import State, LGA, Address
 # Register your models here.
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'city', 'street', 'lga', 'state')
+
+    def state(self, address):
+        return address.lga.state
 
 
 @admin.register(State)
--- location/models.py
+++ location/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 # Create your models here.
 
 
@@ -16,3 +17,26 @@ class LGA(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class AddressManager(models.Manager):
+    def get_address_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+        return Address.objects.filter(content_type=content_type, object_id=obj_id)
+
+
+class Address(models.Model):
+    objects = AddressManager()
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    lga = models.ForeignKey(LGA, on_delete=models.CASCADE)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField(unique=True)
+    content_object = GenericForeignKey()
+
+    class Meta:
+        verbose_name_plural = 'Addresses'
+
+    def __str__(self) -> str:
+        return self.street + ' ' + self.city
